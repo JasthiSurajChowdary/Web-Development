b45387ab1d32d774c53f5610711ea3338f7aaa35
JasthiSurajChowdary <surajchowdaryjasthi@gmail.com>
2026-01-11 08:42:48 +0530

    Add files via upload

---
+++ Library Management System/library/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2024 Online Library Management System |<a href="https://github.com/surajjasthi2000" target="_blank"  > Designed by : Suraj Chowdary</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
